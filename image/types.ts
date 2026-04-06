5acda6427501e059f874895edb169c452e47f9a9
Andrew Wittrock <andy@wittrock.us>
2026-04-06 00:11:35 -0500

    Initial commit

---
+++ image/types.ts
@@ -0,0 +1,10 @@
+export interface IGalleryImage {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    // Add additional fields here
+}
+
+export interface NewGalleryImage {
+    // Add create fields here
+}
